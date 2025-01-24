4d26b74705851f0f33745da5c1b16433ea22f8d1
Sarah Schneider <mureb_rosa@hotmail.com>
2025-01-23 22:35:53 -0300

    feat: kanban board

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,29 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks_count = Task.count
+
+    # Defina valores padrão vazios para as variáveis
+    @pending = Task.where(status: "pending")
+    @completed = Task.where(status: "completed")
+    @in_progress = Task.where(status: "in-progress")
+
+    # Aplica o filtro se params[:status] for fornecido
+    if params[:status].present?
+      case params[:status]
+      when "pending"
+        @pending = @pending
+        @completed = Task.none
+        @in_progress = Task.none
+      when "completed"
+        @completed = @completed
+        @pending = Task.none
+        @in_progress = Task.none
+      when "in-progress"
+        @in_progress = @in_progress
+        @pending = Task.none
+        @completed = Task.none
+      end
+    end
   end
+
 end
