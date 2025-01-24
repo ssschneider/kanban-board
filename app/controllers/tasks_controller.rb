a2fdbdd45b9476ea9bd2fa68c12e508939656a0d
Sarah Schneider <mureb_rosa@hotmail.com>
2025-01-24 15:01:43 -0300

    feat: adjust form to render partial correctly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -79,7 +79,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :description, :status)
   end
 
   def set_task
